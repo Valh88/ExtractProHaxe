6eecd9df865a3abcd0f46361731fb17089475755
vano <simplmd@gmail.com>
2026-08-25 15:27:01 +0700

    up

--- client/src/extract/design/PlayerChip.hx
+++ client/src/extract/design/PlayerChip.hx
@@ -3,22 +3,40 @@ package extract.design;
 import h2d.Flow;
 import h2d.domkit.Object;
 
-/** A single player status chip (READY / WAITING / NOT READY). */
 @:uiComp("player-chip")
 class PlayerChip extends Flow implements Object
 {
-	@:p public var label : String = "";
+	public static inline var READY = "ready";
+	public static inline var WAITING = "waiting";
+	public static inline var NOT_READY = "notready";
 
 	static var SRC =
-		<player-chip>
-			<flow class="chip-icon"/>
-			<text id="status" class="player-status" x="6" y="70"/>
+		<player-chip class="chip">
+			<flow class="chip-bg" x="2" y="2">
+				<flow class="chip-frame" x="10" y="8">
+					<flow class="chip-icon" x="2" y="2"/>
+				</flow>
+			</flow>
+			<text id="status" class="status" x="12" y="86"/>
 		</player-chip>;
 
 	public function new(?parent)
 	{
 		super(parent);
 		initComponent();
-		status.text = label;
+		setStatus(READY);
+	}
+
+	public function setStatus(s : String)
+	{
+		for (c in [READY, WAITING, NOT_READY])
+			this.dom.removeClass("chip-" + c);
+		this.dom.addClass("chip-" + s);
+		status.text = switch (s)
+		{
+			case READY: "READY";
+			case WAITING: "WAITING";
+			default: "NOT READY";
+		};
 	}
 }
--- client/src/extract/design/ReadyPanel.hx
+++ client/src/extract/design/ReadyPanel.hx
@@ -2,12 +2,8 @@ package extract.design;
 
 import h2d.Flow;
 import h2d.domkit.Object;
+import extract.design.PlayerChip;
 
-/**
-	Bottom-left "READY" panel with three player chips, built from the
-	Lunacy `CheckReadingDesign` frame. Status colors:
-	READY #1ACC19, WAITING #B3B319, NOT READY #B33333.
-**/
 @:uiComp("ready-panel")
 class ReadyPanel extends Flow implements Object
 {
@@ -15,30 +11,34 @@ class ReadyPanel extends Flow implements Object
 		<ready-panel>
 			<flow class="ready-inner" x="2" y="2">
 				<text id="title" class="ready-title" x="296" y="12"/>
-				<flow class="player-group" x="135" y="45">
-					<flow class="chip chip-ready" x="0" y="0">
-						<flow class="chip-frame" x="12" y="10"/>
-						<text id="s1" class="status-ready" x="40" y="86"/>
-					</flow>
-					<flow class="chip chip-waiting" x="82" y="0">
-						<flow class="chip-frame" x="12" y="10"/>
-						<text id="s2" class="status-waiting" x="31" y="86"/>
-					</flow>
-					<flow class="chip chip-notready" x="164" y="0">
-						<flow class="chip-frame" x="12" y="10"/>
-						<text id="s3" class="status-notready" x="22" y="86"/>
-					</flow>
-				</flow>
+				<flow id="playerGroup" class="player-group" x="135" y="45"/>
 			</flow>
 		</ready-panel>;
+
+	var chipCount : Int = 0;
 
 	public function new(?parent)
 	{
 		super(parent);
 		initComponent();
 		title.text = "READY";
-		s1.text = "READY";
-		s2.text = "WAITING";
-		s3.text = "NOT READY";
+		addChip(PlayerChip.READY);
+		addChip(PlayerChip.WAITING);
+		addChip(PlayerChip.NOT_READY);
+	}
+
+	public function addChip(status : String) : PlayerChip
+	{
+		var chip = new PlayerChip(playerGroup);
+		chip.x = chipCount * 82;
+		chipCount++;
+		chip.setStatus(status);
+		return chip;
+	}
+
+	public function clearChips()
+	{
+		playerGroup.removeChildren();
+		chipCount = 0;
 	}
 }
