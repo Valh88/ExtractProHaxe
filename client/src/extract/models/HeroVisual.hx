95f11e6147950eba949cd99bca2ca1c51cfef20e
vano <simplmd@gmail.com>
2026-09-17 20:35:04 +0700

    Update HeroVisual.hx

--- client/src/extract/models/HeroVisual.hx
+++ client/src/extract/models/HeroVisual.hx
@@ -46,5 +46,7 @@ class HeroVisual extends Object
 		mainWeapon.x = 0.2;
 		mainWeapon.y = -0.15;
 		mainWeapon.z = -0.4;
+		// FBX model faces +X; rotate -90° around Y so it points forward (-Z)
+		mainWeapon.setRotation(-Math.PI / 2, -Math.PI / 2, 0);
 	}
 }
